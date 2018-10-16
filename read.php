4ea66b6e2c0670d03459534cfbb10a2b2d8c19ca
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2018-10-16 02:33:48 -0300

    Improvement of read.php with some aesthetic details and functionalities

--- read.php
+++ read.php
@@ -4,10 +4,16 @@
 	document.title = "Show contacts";
 </script>
 <?php
+	// connection
 	require_once 'db_connect.php';
+
+	// header
 	require_once 'header.php';
+
+	// session initialization
 	session_start();
 
+	// show user name
 	if (isset($_SESSION['id-user'])) {
 		$id = $_SESSION['id-user'];
 		$sql = "SELECT * FROM users WHERE id = '$id'";
@@ -19,10 +25,33 @@
 
 	<div class="row">
   		<div class="col s12 m6 push-m3">
-			<h2 class="light">Hello <?php echo $login; ?></h2>
-			<a href="logout.php" class="btn">Log out</a>
+  			<h3 class="light">Show contacts</h3>
+  			<table class="striped">
+  				<thead>
+  					<tr>
+  						<td>Name</td>
+  						<td>Surname</td>
+  						<td>Email</td>
+  						<td>Phone</td>
+  					</tr>
+  				</thead>
+  				<tbody>
+  					<tr>
+  						<td>Hamilton</td>
+  						<td>Gonçalves Jr</td>
+  						<td>hamiltonjr2010@gmail.com</td>
+  						<td>19997930712</td>
+  						<td><a href="" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
+  						<td><a href="" class="btn-floating red"><i class="material-icons">delete</i></a></td>
+  					</tr>  					
+  				</tbody>
+  			</table><br/>
+  			<a href="" class="btn">Add contact</a>
+  			<a href="index.html" class="btn">Home</a>
 		</div>
 	</div>
+
 <?php
+	// footer
 	require_once 'footer.php';
 ?>
